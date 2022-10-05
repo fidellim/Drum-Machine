81434494415c202f6dc4a0b788511c5dc24a8a32
fidellim <lim.fidel@gmail.com>
2022-10-05 23:34:10 +0400

    add speaker icons

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -1,16 +1,22 @@
 import React, { useState, useContext } from 'react'
 import { Context } from '../App'
+import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid'
 
 const RangeSlider = () => {
     const { volume, setVolume } = useContext(Context)
 
     const handleChange = (e) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setVolume(e.target.value)
     }
 
     return (
-        <div>
+        <div className=" flex items-center gap-2">
+            {volume > 0 ? (
+                <SpeakerWaveIcon className="h-10 text-darkBlue" />
+            ) : (
+                <SpeakerXMarkIcon className="h-10 text-darkBlue" />
+            )}
             <input
                 id="default-range"
                 type="range"
