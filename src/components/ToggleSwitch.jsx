3622388b0cbfb3c32a4296567be8512f5ee6e495
fidellim <lim.fidel@gmail.com>
2022-10-06 00:29:15 +0400

    set state of toggles

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,9 +1,4 @@
-import { useContext } from 'react'
-import { Context } from '../App'
-
-const ToggleSwitch = ({ name, setState }) => {
-    const { volume, togglePower } = useContext(Context)
-
+const ToggleSwitch = ({ name, setState, state }) => {
     const handleClick = (e) => {
         // e.preventDefault()
         // e.stopPraaopagation()
@@ -21,9 +16,10 @@ const ToggleSwitch = ({ name, setState }) => {
         >
             <input
                 type="checkbox"
-                value=""
+                checked={state}
                 id={name}
                 onClick={handleClick}
+                onChange={handleClick}
                 className="sr-only peer"
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
