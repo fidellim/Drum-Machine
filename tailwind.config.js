45b2d68ddcb58cc77a209b58ef5807fb53fc1d9e
fidellim <lim.fidel@gmail.com>
2022-10-05 23:32:24 +0400

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                darkBlue: 'var(--darkBlue)',
+                darkGreen: 'var(--darkGreen)',
+                lightGray: 'var(--lightGray)',
+                lightBlue: 'var(--lightBlue)',
+            },
+        },
+    },
+    plugins: [],
 }
